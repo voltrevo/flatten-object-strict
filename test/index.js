1eaf541f9c88d135ddb5bf6828937a4e6505f688
Andrew Morris <voltrevo@gmail.com>
2015-09-20 16:39:52 +1000

    Add test for flat objects

--- test/index.js
+++ test/index.js
@@ -10,4 +10,14 @@ describe('flatten-object-strict', function() {
   it('an empty object produces an empty object', function() {
     assert.deepEqual(flattenObjectStrict({}), {});
   });
+
+  it('a object that\'s already flat doesn\'t change', function() {
+    [
+      { a: 'a', b: [1, 2, 3], c: function() {} },
+      { foo: 'bar', baz: 'boom' },
+      { one: 'one', two: 'two', three: 'three', four: 'four', five: 'five' }
+    ].forEach(function(flatObject) {
+      assert.deepEqual(flattenObjectStrict(flatObject), flatObject);
+    });
+  });
 });
